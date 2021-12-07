7c02860367447b62d01de0b690310dd084da4548
Manuela Briere <manuelatgkb@gmail.com>
2021-12-07 18:06:58 -0300

    ATP78 inicial

---
+++ ATP77D/src/app/tipocliente-lista/tipocliente-lista.component.ts
@@ -0,0 +1,23 @@
+import { TipoclienteService } from '../services/tipocliente.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tipocliente-lista',
+  templateUrl: './tipocliente-lista.component.html',
+  styleUrls: ['./tipocliente-lista.component.css']
+})
+export class TipoclienteListaComponent implements OnInit {
+  lista: any[];
+
+  constructor(private TipoclienteService: TipoclienteService) {
+    this.lista = [];
+    this.listar();
+   }
+
+  ngOnInit(): void {
+  }
+
+  private listar(){
+    this.lista = this.TipoclienteService.listar();
+  }
+}
