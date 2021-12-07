323159982cd16b08cdee4e1d9b0cae680e2cc5e7
Manuela Briere <manuelatgkb@gmail.com>
2021-12-07 14:39:58 -0300

    refazendoatp77 para solucionar erro do angular

---
+++ ATP77D/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ClienteListaComponent } from './cliente-lista/cliente-lista.component';
+import { ClienteCadastroComponent } from './cliente-cadastro/cliente-cadastro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClienteListaComponent,
+    ClienteCadastroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
